251d494100b34732a3874a0dd8abb35672f4d038
Gnon Déolinda Bio Bogore <139719649+Deolinda1506@users.noreply.github.com>
2024-05-27 14:12:08 +0200

    Update 0-rain.py

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,25 +1,33 @@
 #!/usr/bin/python3
+"""Module for the rain function
+"""
+
 
 def rain(walls):
-    if not walls:
+    """Function to find the maximum rain collected
+    by a series of walls
+    """
+    n = len(walls)
+
+    if n == 0:
         return 0
 
-    left, right = 0, len(walls) - 1
-    left_max, right_max = 0, 0
-    water_trapped = 0
+    total_volume = 0
 
-    while left <= right:
-        if walls[left] < walls[right]:
-            if walls[left] >= left_max:
-                left_max = walls[left]
-            else:
-                water_trapped += left_max - walls[left]
-            left += 1
-        else:
-            if walls[right] >= right_max:
-                right_max = walls[right]
-            else:
-                water_trapped += right_max - walls[right]
-            right -= 1
+    # Find the highest element to the left of each element
+    highest_left = [0] * n
+    highest_left[0] = walls[0]
+    for i in range(1, n):
+        highest_left[i] = max(highest_left[i - 1], walls[i])
 
-    return water_trapped
+    # Find the highest wall to the right of each
+    highest_right = [0] * n
+    highest_right[-1] = walls[-1]
+    for i in range(n - 2, -1, -1):
+        highest_right[i] = max(highest_right[i + 1], walls[i])
+
+    # Calculate the accumulated water element by element
+    for i in range(0, n):
+        total_volume += min(highest_left[i], highest_right[i]) - walls[i]
+
+    return total_volume
